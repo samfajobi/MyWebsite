2f2b861811b9213aa32bbf6903cb436430d55cc9
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-02 14:02:41 +0100

    Added Resume Icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import {  faFolderOpen} from '@fortawesome/free-solid-svg-icons'
+import { Tooltip as ReactTooltip } from 'react-tooltip'
+import { Link } from 'react-router-dom';
 
 // const Baller = styled.div`
 //  font-size: 56px;
@@ -17,6 +20,9 @@ const NavWrapper = styled.div`
  
 `
 
+
+
+
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
@@ -25,35 +31,37 @@ const Navbar = () => {
     <NavWrapper>
 
    
-    <div className='h-[80px] bg-gradient-to-t from-indigo-200  bg-blue-400'>
+    <div className='h-[80px] bg-gradient-to-t from-indigo-200  bg-white'>
         <div className=' h-20 w-full bg-black fixed top-0'>
-          <div className='container mx-auto py-6 flex items-center justify-between'>
+            <div className='container mx-auto py-6 flex items-center justify-between'>
              <div>
               <img src="" alt="" />
               <div className='text-2xl font-bold text-white'>Samuel.</div>
              </div>
-            <div>
-              <ul className='hidden md:flex space-x-10 text-white font-bold text-l'  >
-                <li className='hover:text-gray-500' >
-                  <a href="/">Home</a>
-                </li>
-                <li className='hover:text-gray-500' >
-                  <a href="/">About</a>
-                </li>
-                <li className='hover:text-gray-500' >
-                  <a href="/">Work</a>   
-                </li>
-                <li className='hover:text-gray-500' >
-                  <a href="/">Skills</a> 
-                </li>  
-                <li className='hover:text-gray-500' >
-                  <a href="/">Contact</a>
-                </li>
-              </ul> 
-            </div> 
              <div>
-              <img src="./test.jpg" alt="" className='hidden md:block w-8 cursor-pointer' />
-              <div onClick={handleClick}  className='space-y-6 cursor-pointer md:hidden'>
+                <ul className='hidden md:flex space-x-10 text-white font-bold text-l'  >
+                    <li className='hover:text-gray-200' >
+                    <a href="/">Home</a>
+                    </li>
+                    <li className='hover:text-gray-200' >
+                    <a href="/">About</a>
+                    </li>
+                    <li className='hover:text-gray-200' >
+                    <a href="/">Work</a>   
+                    </li>
+                    <li className='hover:text-gray-200' >
+                    <a href="/">Skills</a> 
+                    </li>  
+                    <li className='hover:text-gray-200' >
+                    <a href="/">Contact</a>
+                    </li>
+                </ul> 
+              </div> 
+             <div>
+              <div className='text-white hidden md:block  cursor-pointer'>
+                <span className='text-1xl text-white'>Resume</span> <FontAwesomeIcon icon={faFolderOpen} />
+             </div>   
+              <div onClick={handleClick}  className=' text-white space-y-6 cursor-pointer md:hidden'>
                 { !nav ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faXmark} />}
               </div> 
             </div> 
