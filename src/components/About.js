7721f6e965099aefad4b6480c393063d729986c7
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-03 21:25:36 +0100

    Updates About me Sections

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,327 @@
-import React from 'react'
+import { AnimatePresence, motion } from "framer-motion";
+import { data, useTwitter } from "../../data/newsSlider";
+import OpenLinkIcon from "../../assets/svg/OpenLinkIcon";
+import TwitterIcon from "../../assets/svg/TwitterIcon";
+import { useEffect, useState } from "react";
+import FaderWrap from "../FaderWrap";
+import NextIcon from "../../assets/svg/NextIcon";
+import { client } from "../../client";
+import NewsWrapper from "./style/newsStyle";
 
-const about = () => {
+
+
+
+
+const NewsSlider = (props) => {
+  const [newsIndex, setNewsIndex] = useState(0);
+  const [socialIndex, setSocialIndex] = useState(0);
+  const [isScroll, setIsScroll] = useState(true);
+  const [isTweetScroll, setIsTweetScroll] = useState(true);
+
+  const [tweets, isError, isLoading] = useTwitter();
+
+  const [news, setNews] = useState([ ]);
+
+  const ContentVaraint = {
+    rest: {
+      x: isScroll ? 300 : -300,
+    },
+
+    hover: {
+      x: 0,
+      transition: {
+        type: "tween",
+        ease: "easeOut",
+        duration: 0.8,
+      },
+    },
+
+    rest2: {
+      x: isTweetScroll ? 300 : -300,
+    },
+
+    hover2: {
+      x: 0,
+      transition: {
+        type: "tween",
+        ease: "easeOut",
+        duration: 0.8,
+      },
+    },
+
+    borderRest: {
+      y: 50,
+      opacity: 0,
+    },
+
+    borderAnimate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: 1,
+        type: "tween",
+        ease: "easeOut",
+        duration: 0.5,
+      },
+    },
+  };
+
+  useEffect(() => {
+    if (news.length === 0) {
+      client
+        .fetch(
+          `*[_type == "globalnews"]{
+          _id,
+          altText,
+          descText,
+          details,
+          headerText,
+          img{
+            asset->{
+              _id,
+              url
+            },
+          },
+          images,
+        }`
+        )
+        .then((data) => setNews(data))
+        .catch(console.error);
+    }
+  }, [news.length]);
+
+  //news slider control
+  const slideNewsRight = () => {
+    setNewsIndex((newsIndex + 1) % news?.length); // increases index by 1
+    // setSocialIndex((socialIndex + 1) % [0, 1, 2].length);
+  };
+  const slideSocials = () => {
+    setSocialIndex((socialIndex + 1) % [0, 1, 2].length);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setNewsIndex((newsIndex + 1) % news?.length);
+    }, 6000);
+    const indexTimer = setTimeout(() => {
+      setSocialIndex((socialIndex + 1) % [0, 1, 2].length);
+    }, 6000);
+
+    return () => {
+      clearTimeout(timer);
+      clearTimeout(indexTimer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [newsIndex, socialIndex, news.length]);
+
+
+   useEffect(() => {
+    client
+      .fetch(
+        `*[_type == "globalnews"]{
+          _id,
+          altText,
+          descText,
+          details,
+          headerText,
+          img{
+            asset->{
+              _id,
+              url
+            },
+          },
+          images,
+        }`
+      )
+      .then((data) => setNews(data))
+      .catch(console.error);
+  }, []);
+
+  
+
   return (
-    <div>
-        <div>
-            <h1>You Wanna Know more About me? Come On</h1>
-        </div>
-    </div>
-  )
-}
+    <FaderWrap>
+      <NewsWrapper className='px-4 py-2 lg:px-16 relative mb-10' {...props}>
+        {news.length > 0 && (
+          <div className='relative flex justify-center'>
+            <ol className='flex w-10 justify-between h-2 mini-caurosel lg:ml-56 absolute z-10'>
+              <li
+                className={`cursor-pointer inline-block w-1/5 border border-white ${
+                  newsIndex === 0 ? "bg-white" : ""
+                }`}
+                onClick={() => {
+                  setNewsIndex(0);
+                  setIsScroll(false);
+                }}
+              ></li>
+              <li
+                className={`cursor-pointer inline-block w-1/5 border border-white ${
+                  newsIndex === 1 ? "bg-white" : ""
+                }`}
+                onClick={() => {
+                  setNewsIndex(1);
+                  if (newsIndex > 1) {
+                    setIsScroll(false);
+                  } else {
+                    setIsScroll(true);
+                  }
+                }}
+              ></li>
+              <li
+                className={`cursor-pointer inline-block w-1/5 border border-white ${
+                  newsIndex === 2 ? "bg-white" : ""
+                }`}
+                onClick={() => {
+                  setNewsIndex(2);
+                  setIsScroll(true);
+                }}
+              ></li>
+            </ol>
+            <div className='lg:justify-center lg:flex-row relative 2xl:w-8/12 '>
+              <div className='flex flex-col lg:justify-center lg:flex-row overflow-hidden relative'>
+                <div className='relative overflow-hidden section-1 w-screen bg-blue-400 ' >
+                  <AnimatePresence>
+                    <motion.section
+                      className='flex flex-col lg:flex-row relative'
+                      initial='rest'
+                      animate='hover'
+                      variants={ContentVaraint}
+                      key={news[newsIndex]?._id}
+                      exit={{ visibility: "hidden" }}
+                    >
+                      <div className=' w-full lg:w-10/12 cont-height'>
+                        <img
+                          src={news[newsIndex]?.img.asset.url}
+                          alt={news[newsIndex]?.altText}
+                          className='h-full object-cover w-full'
+                        />
+                      </div>
+                      <div className='bg-white w-full lg:w-10/12 arrow px-4 py-8'>
+                        <p className='uppercase text-xs text-blue-500 mb-14'>
+                          press release
+                        </p>
+                        <motion.header className='relative flex flex-col'>
+                          <h1 className='text-gray-700 text-xl font-bold mb-6'>
+                            {news[newsIndex]?.headerText}
+                          </h1>
+                          <section className='flex justify-center items-center'>
+                            <div className='flex w-3/4 h-auto'>
+                              <motion.div
+                                initial='borderRest'
+                                animate='borderAnimate'
+                                variants={ContentVaraint}
+                                className='border-brand-yellow border-l mr-2'
+                              ></motion.div>
+                              <div className=''>
+                                <p className='text-gray-600 text-sm font-medium mb-2'>
+                                  {news[newsIndex]?.descText}
+                                </p>
+                                <a
+                                  href={`articles/${newsIndex}`}
+                                  className='flex items-center justify-end'
+                                >
+                                  <span className='mr-2 text-brand-blue font-bold'>
+                                    Learn More
+                                  </span>
+                                  <NextIcon color='#2659A2' />
+                                </a>
+                              </div>
+                            </div>
+                          </section>
+                        </motion.header>
+                      </div>
+                    </motion.section>
+                  </AnimatePresence>
+                </div>
+                {/* <section className='section-2 relative mt-   8 lg:mt-0'> */}
+                  {/* <ol className='flex w-10 justify-between h-2 social-caurosel absolute z-10'>
+                    {[0, 1, 2].map((i) => (
+                      <li
+                        key={i}
+                        className={`cursor-pointer inline-block w-1/5 border border-white ${
+                          socialIndex === i ? "bg-white" : ""
+                        }`}
+                        onClick={() => {
+                          setSocialIndex(i);
+                          if (i < 1) {
+                            setIsTweetScroll(false);
+                          } else {
+                            setIsTweetScroll(true);
+                          }
+                        }}
+                      ></li>
+                    ))}
+                  </ol> */}
 
-export default about   
+                  {/* <a
+                    href='https://twitter.com/search?q=mojec'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='section-2 relative mb-8 lg:mb-0 flex pl-8 pr-4 py-8 flex-col'
+                    // className='flex pl-8 pr-4 py-8 flex-col'
+                  >
+                    <TwitterIcon className='mb-12' />
+                    <div className='overflow-hidden'>
+                      {isLoading && (
+                        <p className='text-white text-center'>
+                          Loading tweets....
+                        </p>
+                      )}
+                      {isError && (
+                        <p className='text-white text-center'>
+                          Error loading tweets!
+                        </p>
+                      )}
+                      {tweets && tweets.length < 1 ? (
+                        <p className='text-white text-center'>
+                          No tweet to display!!
+                        </p>
+                      ) : (
+                        <AnimatePresence>
+                          <motion.section
+                            className='flex h-auto relative'
+                            initial='rest2'
+                            animate='hover2'
+                            variants={ContentVaraint}
+                            key={tweets && tweets[socialIndex].id}
+                            exit={{
+                              visibility: "hidden",
+                            }}
+                          >
+                            <motion.div
+                              initial='borderRest'
+                              animate='borderAnimate'
+                              variants={ContentVaraint}
+                              className='border-white border-l h-auto mr-2'
+                            ></motion.div>
+                            <div className=''>
+                              <p className='text-sm font-medium text-white mb-1'>
+                                {tweets && tweets[socialIndex].text}
+                              </p>
+                              <p className='text-white italic text-xs'>
+                                {tweets &&
+                                  tweets[socialIndex].created_at.substr(0, 10)}
+                              </p>
+                              <div className='flex justify-end items-center absolute right-0 top-64'>
+                                <OpenLinkIcon />
+                              </div>
+                            </div>
+                          </motion.section>
+                        </AnimatePresence>
+                      )}
+                    </div>
+                  </a> */}
+                {/* </section> */} 
+              </div>
+              <h2 className='font-bold capitalize text-3xl inline-block text-gray-600 absolute lg:ml-52 z-10 pl-32'>
+                in the news
+              </h2>
+            </div>
+          </div>
+        )}
+      </NewsWrapper>
+    </FaderWrap>
+  );
+};
+
+export default NewsSlider;
---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Projects = props => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  ) 
+}
+
+
+
+export default Projects
