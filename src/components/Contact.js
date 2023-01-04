86a42c83373f3bcab0288d9f3159fdb9ebf9b851
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-05 00:28:47 +0100

    Update Contact Section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,12 @@ import { EnvelopeIcon } from '@heroicons/react/24/solid'
 
 const contact = () => {
   return (
-    <div className=" h-[400px] flex relative flex-col text-center
+    <div className=" h-[600px] bg-gray-900 flex relative flex-col text-center
      md:text-row items-center px-10 justify-evenly mx-auto">
-       <h2 className="text-center  text-4xl font-semibold">Take Coffee and Chat With Me!!!</h2>
+       <h2 className="text-center text-white text-4xl font-semibold">Take Coffee and Chat With Me!!!</h2>
       
-        <div className="space-x-10  ">
-            <div className='flex flex-col md:space-x-6 md:flex-row'>
+        <div className=" space-y-10 ">
+            <div className='flex flex-col items-center md:space-x-6 md:flex-row'>
               <div className='rounded-md w-[300px] bg-slate-200 flex pt-4 pb-4 align-center items-center space-x-4 justify-center'>
                 <PhoneIcon className='w-7 h-7'/>
                 <h4>08153382359</h4>
@@ -17,12 +17,20 @@ const contact = () => {
                 <EnvelopeIcon className='w-7 h-7'/>
                 <h4>olusegunfajobi12@gmail.com</h4>
               </div>
-            </div> 
-            <form>
-               <div> 
-               </div> 
-            </form>
-             
+            </div>
+            <div>
+              <form className='flex flex-col space-y-6'>
+                <div className='flex space-x-3' > 
+                  <input type="text" />
+                  <input type="text" />
+                </div> 
+
+                <input type="text" />
+                
+                <textarea />
+                <button className='rounded-md font-bold  py-5 bg-slate-300'>Submit</button>
+              </form>  
+            </div>          
         </div>
     </div>
   )
