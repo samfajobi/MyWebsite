3b6bff9d8c2cd72f415c8fa2929f19d85e623525
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-04 22:39:42 +0100

    Update Skills Sectio

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,34 +1,20 @@
 
 
 
+const skills = () => {
+  return (
+    <div className="h-[700px] mt-16 bg-slate-100"> 
+        <h1 className="text-4xl text-center ">Skills & Experience</h1>
+        <div className="flex flex-row">
+            <div> 
+                Div for skills Icons
+            </div>
+            <div>
+               Div for Experience 
+            </div>
+        </div>
+    </div>
+  )
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const skills = () => {
-//   return (
-//     <div className="h-[700px] mt-16 bg-slate-100"> 
-//         <h1 className="text-4xl text-center ">Skills & Experience</h1>
-//         <div className="flex flex-row">
-//             <div> 
-//                 Div for skills Icons
-//             </div>
-//             <div>
-//                Div for Experience 
-//             </div>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default skills
+export default skills
