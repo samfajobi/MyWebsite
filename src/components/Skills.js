c6f50c2eb1afee34ac0385a4d06116260246ff8e
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-06 13:36:48 +0100

    Update Skill Section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,56 +3,58 @@ import JavaScript from '../assets/images/images/skills/javascript.png'
 
 const skills = () => {
   return (
-    <div className="h-[700px] mt-16 bg-slate-100 flex flex-col space-y-10"> 
+    <div className="h-[700px] mt-16 bg-slate-100  space-y-10"> 
         <h1 className="text-4xl text-center text-4xl font-semibold ">Skills & Experience</h1>
-        <div className='space-y-10'>
-          <div className="flex flex-row justify-center space-x-8">
-            <div > 
-               <img className=' bg-slate-200' src={JavaScript}/>
-               <h2 className='text-center'>Javascript</h2>       
-            </div> 
-            <div> 
-               <img className=' bg-slate-200' src={JavaScript}/>
-               <h2 className='text-center'>Javascript</h2>       
-            </div> 
-            <div> 
-               <img className=' bg-slate-200' src={JavaScript}/>
-               <h2 className='text-center'>Javascript</h2> 
-            </div>   
+        <div  className='flex flex-col  md:flex-row justify-center'>
+          <div className='space-y-10 md:mx-10 '>
+            <div className="flex flex-row justify-center space-x-8">
+              <div > 
+                <img className=' bg-slate-200' src={JavaScript}/>
+                <h2 className='text-center'>Javascript</h2>       
+              </div> 
+              <div> 
+                <img className=' bg-slate-200' src={JavaScript}/>
+                <h2 className='text-center'>Javascript</h2>       
+              </div> 
+              <div> 
+                <img className=' bg-slate-200' src={JavaScript}/>
+                <h2 className='text-center'>Javascript</h2> 
+              </div>   
+            </div>
+            <div className="flex flex-row justify-center space-x-8">
+              <div > 
+                <img className=' bg-slate-200' src={JavaScript}/>
+                <h2 className='text-center'>Javascript</h2>       
+              </div> 
+              <div> 
+                <img className=' bg-slate-200' src={JavaScript}/>
+                <h2 className='text-center'>Javascript</h2>       
+              </div> 
+              <div> 
+                <img className=' bg-slate-200' src={JavaScript}/>
+                <h2 className='text-center'>Javascript</h2> 
+              </div>   
+            </div>
+            <div className="flex flex-row justify-center space-x-8">
+              <div > 
+                <img className=' bg-slate-200' src={JavaScript}/>
+                <h2 className='text-center'>Javascript</h2>       
+              </div> 
+              <div> 
+                <img className=' bg-slate-200' src={JavaScript}/>
+                <h2 className='text-center'>Javascript</h2>       
+              </div> 
+              <div> 
+                <img className=' bg-slate-200' src={JavaScript}/>
+                <h2 className='text-center'>Javascript</h2> 
+              </div>   
+            </div>
           </div>
-          <div className="flex flex-row justify-center space-x-8">
-            <div > 
-               <img className=' bg-slate-200' src={JavaScript}/>
-               <h2 className='text-center'>Javascript</h2>       
-            </div> 
-            <div> 
-               <img className=' bg-slate-200' src={JavaScript}/>
-               <h2 className='text-center'>Javascript</h2>       
-            </div> 
-            <div> 
-               <img className=' bg-slate-200' src={JavaScript}/>
-               <h2 className='text-center'>Javascript</h2> 
-            </div>   
-          </div>
-          <div className="flex flex-row justify-center space-x-8">
-            <div > 
-               <img className=' bg-slate-200' src={JavaScript}/>
-               <h2 className='text-center'>Javascript</h2>       
-            </div> 
-            <div> 
-               <img className=' bg-slate-200' src={JavaScript}/>
-               <h2 className='text-center'>Javascript</h2>       
-            </div> 
-            <div> 
-               <img className=' bg-slate-200' src={JavaScript}/>
-               <h2 className='text-center'>Javascript</h2> 
-            </div>   
-          </div>
-        </div>
         
-        <div>
-          Div for Experience 
-        </div>
+          <div>
+            Div for Experience 
+          </div>
+        </div>  
     </div>
   )
 }
