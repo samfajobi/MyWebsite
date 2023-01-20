b15fbc68d4fb4d40e99d402a66de5efc6240cb90
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-21 00:51:44 +0100

    Projects Updates

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,27 +8,34 @@ export const awards = [
 	},
 	{
 		id: 2,
+		type: 'Social Media App',
+		desc: 'A Social Media application',
+		toWeb: "precise-tracker.vercel.app",
+		toGitHub: "https://github.com/samfajobi/precise-tracker-frontend"
+	},
+	{
+		id: 2,
 		type: 'Reservation WebApp',
 		desc: 'A Reservation Web App built with the MERN stack for making space reservation',
 		toWeb: "https://miamibooking.vercel.app/",
 		toGitHub: "https://github.com/samfajobi/MiamiReservation-backend"
 	},
 	{
-		id: 3,
+		id: 4,
 		type: 'Business Website',
 		desc: 'A Business Website built with React.js for a sales company',
 		toWeb: "https://christabossglobal.netlify.app/",
 		toGitHub: "https://github.com/samfajobi/ChiristabossWeb"
 	},
 	{
-		id: 4,
+		id: 5,
 		type: 'Weather App',
 		desc: 'A Weather  Application built with React.js',
 		toWeb: "https://predictweatherapp.netlify.app/",
 		toGitHub: "https://github.com/samfajobi/MyWeatherApp"
 	},
 	{
-		id: 4,
+		id: 6,
 		type: 'E-commerce Store',
 		desc: 'Web Application built with MERN',
 		toWeb: "https://predictweatherapp.netlify.app/",
